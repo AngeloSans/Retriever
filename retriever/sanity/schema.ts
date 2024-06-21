6f50e8e1f91926f8cac32c3e5845cc06114ded9f
Angelo Santos <gabrielange883@gmail.com>
2024-06-21 12:27:55 -0300

    21062024-Sanity implementations, Schemas DB

--- retriever/sanity/schema.ts
+++ retriever/sanity/schema.ts
@@ -29,5 +29,54 @@ export const schema: { types: SchemaTypeDefinition[] } = {
         }),
       ],
     }),
+    defineType({
+      name: 'Relato',
+      type: 'document',
+      title: 'Relato',
+      fields: [
+        defineField({
+          name: 'user',
+          title: 'Title',
+          type: 'string',
+        }),
+        defineField({
+          name: 'instagram',
+          title: 'Content',
+          type: 'string',
+        }),
+        defineField({
+          name: 'Mensage',
+          title: 'mensage',
+          type: 'string',
+        }),
+        defineField({
+          name: 'date',
+          title: 'Date',
+          type: 'date',
+        }),
+      ],
+    }),
+    defineType({
+      name: 'RetrieverAcademic',
+      type: 'document',
+      title: 'Relato',
+      fields: [
+        defineField({
+          name: 'title',
+          title: 'Title',
+          type: 'string',
+        }),
+        defineField({
+          name: 'autor',
+          title: 'autor',
+          type: 'string',
+        }),
+        defineField({
+          name: 'previous',
+          title: 'previous',
+          type: 'string',
+        }),
+      ],
+    }),
   ],
 };
