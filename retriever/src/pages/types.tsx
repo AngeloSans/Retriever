1ff7ee06e750f7aed2cae5e642fa9fb5d6020d21
Angelo Santos <gabrielange883@gmail.com>
2024-07-26 08:55:54 -0300

    26072024-Types

--- retriever/src/pages/types.tsx
+++ retriever/src/pages/types.tsx
@@ -5,7 +5,7 @@ import Footer from '../components/Footer';
 import '../app/globals.css';
 
 const Types = () => {
-    const [selectedType, setSelectedType] = useState<string | null>(null);
+    const [selectedType, setSelectedType] = useState<string | null>('Social');
 
     const handleTypeClick = (type: string) => {
         setSelectedType(type);
@@ -26,13 +26,17 @@ const Types = () => {
 
             <main className="container mx-auto p-4">
                 <section className="mb-8">
-                    <h2 className="text-2xl font-semibold mb-4 text-center text-purple-600">Tipos de Ansiedade</h2>
-                    <div className="flex flex-wrap justify-center gap-4">
-                        {['Social', 'Pânico', 'TOC', 'TDAH', 'Pânico', 'Pânico', 'Pânico'].map((type, index) => (
+                    <h2 className="text-4xl text-black text-center font-semibold mt-40">Ansiedade Classificada por Tipos</h2>
+                    <h2 className="text-xl font-medium mb-4 text-center text-gray-800 mt-6">
+                        Todo mundo já sentiu apreensão e ansiedade em algum momento da vida. Seja pela expectativa de uma prova, a apresentação de um projeto ou algum compromisso importante. Essa é a ansiedade comum do dia a dia, da qual tanto ouvimos falar. Mas você sabia que existem diferentes tipos de ansiedade?
+                    </h2>
+
+                    <div className="flex justify-center gap-4 border-b-2 border-gray-200 mb-6">
+                        {['Social', 'Pânico', 'TOC', 'TDAH'].map((type, index) => (
                             <button
                                 key={index}
-                                className={`p-4 rounded-full transition duration-300 ease-in-out ${
-                                    selectedType === type ? 'bg-purple-600 text-white' : 'bg-gray-300 text-gray-700 hover:bg-purple-300'
+                                className={`p-4 text-lg transition duration-300 font-semibold ease-in-out ${
+                                    selectedType === type ? 'border-b-4 border-black text-black' : 'text-back hover:text-black'
                                 }`}
                                 onClick={() => handleTypeClick(type)}
                             >
@@ -45,26 +49,32 @@ const Types = () => {
                 <section>
                     {selectedType === 'Social' && (
                         <article className="mb-8">
-                            <img src="/path/to/image1.jpg" alt="Transtorno de Ansiedade Social" className="w-full mb-4 rounded-lg shadow-md" />
-                            <h3 className="text-xl font-semibold">Transtorno de Ansiedade Social: o que é, sintomas e tratamento</h3>
-                            <p className="mt-2 text-gray-700">Descrição detalhada sobre o transtorno de ansiedade social.</p>
+                            <img src="/ImagesRetriever/ansiedadesocialtypes.jpg" alt="Transtorno de Ansiedade Social" className="max-w-md h-64 mx-auto mb-4 rounded-lg shadow-md object-cover" />
+                            <h3 className="text-xl font-semibold text-black">Transtorno de Ansiedade Social: o que é, sintomas e tratamento</h3>
+                            <p className="mt-2 text-black">Descrição detalhada sobre o transtorno de ansiedade social.</p>
                         </article>
                     )}
                     {selectedType === 'Pânico' && (
                         <article className="mb-8">
-                            <img src="/path/to/image2.jpg" alt="Ansiedade em Crianças" className="w-full mb-4 rounded-lg shadow-md" />
-                            <h3 className="text-xl font-semibold">Entenda o que é ansiedade social e como se manifesta nas crianças</h3>
-                            <p className="mt-2 text-gray-700">Descrição detalhada sobre a ansiedade em crianças.</p>
+                            <img src="/ImagesRetriever/panicotypes.jpg" alt="Ansiedade em Crianças" className="max-w-md h-64 mx-auto mb-4 rounded-lg shadow-md object-cover" />
+                            <h3 className="text-xl font-semibold text-black">Entenda o que é ansiedade social e como se manifesta nas crianças</h3>
+                            <p className="mt-2 text-black">Descrição detalhada sobre a ansiedade em crianças.</p>
                         </article>
                     )}
                     {selectedType === 'TOC' && (
                         <article className="mb-8">
-                            <img src="/path/to/image3.jpg" alt="Ansiedade e Fobia Social" className="w-full mb-4 rounded-lg shadow-md" />
-                            <h3 className="text-xl font-semibold">Fobia social quando o sofrimento pela timidez é tanto que a saída é se esconder</h3>
+                            <img src="/ImagesRetriever/toctypes.jpg" alt="Ansiedade e Fobia Social" className="max-w-md h-64 mx-auto mb-4 rounded-lg shadow-md object-cover" />
+                            <h3 className="text-xl text-black font-semibold">Fobia social quando o sofrimento pela timidez é tanto que a saída é se esconder</h3>
                             <p className="mt-2 text-gray-700">Descrição detalhada sobre ansiedade e fobia social.</p>
                         </article>
                     )}
-
+                    {selectedType === 'TDAH' && (
+                        <article className="mb-8">
+                            <img src="/ImagesRetriever/tdahtypes.jpeg" alt="Ansiedade e Fobia Social" className="max-w-md h-64 mx-auto mb-4 rounded-lg shadow-md object-cover" />
+                            <h3 className="text-xl text-black font-semibold">Fobia social quando o sofrimento pela timidez é tanto que a saída é se esconder</h3>
+                            <p className="mt-2 text-gray-700">Descrição detalhada sobre ansiedade e fobia social.</p>
+                        </article>
+                    )}
                 </section>
             </main>
 
