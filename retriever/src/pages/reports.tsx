5fb2aff6fc4f3f14d4287f11ba1f3ca09a2db656
Angelo Santos <gabrielange883@gmail.com>
2024-08-04 09:53:08 -0300

    04082024-ReportsPage

--- retriever/src/pages/reports.tsx
+++ retriever/src/pages/reports.tsx
@@ -40,18 +40,25 @@ const Reports = ({ relatos }: InferGetStaticPropsType<typeof getStaticProps>) =>
 
             <div className="mt-16 text-center w-full px-4 mt-40">
                 <h2 className="text-3xl font-bold text-purple-900 mb-8">Relatos de Experiências Pessoais</h2>
-                <div className="grid gap-8 max-w-[600px] mx-auto">
+                <div className="grid gap-8 max-w-2xl mx-auto">
                     {relatos.map((relato) => (
-                        <div key={relato._id} className="bg-white border border-gray-200 p-6 rounded-lg shadow-lg">
+                        <div
+                            key={relato._id}
+                            className="bg-white border border-gray-200 p-6 rounded-lg shadow-lg max-w-xs mx-auto"
+                        >
                             <div className="flex items-center space-x-4">
-                                <img src={relato.photo} alt={relato.user} className="w-16 h-16 rounded-full object-cover"/>
+                                <img
+                                    src={relato.photo}
+                                    alt={relato.user}
+                                    className="w-16 h-16 rounded-full object-cover"
+                                />
                                 <div>
                                     <h3 className="text-xl font-bold text-gray-800">{relato.user}</h3>
                                     <p className="text-sm text-gray-600">@{relato.instagram}</p>
                                     <p className="text-sm text-gray-600">{new Date(relato.date).toLocaleDateString()}</p>
                                 </div>
                             </div>
-                            <p className="mt-4 text-lg text-gray-700">{relato.message}</p>
+                            <p className="mt-4 text-sm text-gray-700 max-w-xs break-words">{relato.message}</p>
                         </div>
                     ))}
                 </div>
