bf879e0e0a34bb7243b52e4499eaa528aab35a85
Angelo Santos <gabrielange883@gmail.com>
2024-08-04 09:42:40 -0300

    04082024-Explorer Page

--- retriever/src/components/TopicCard.tsx
+++ retriever/src/components/TopicCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 
 interface TopicCardProps {
-    id: string; 
+    id: string;
     title: string;
     summary: string;
     date: string;
@@ -15,18 +15,18 @@ interface TopicCardProps {
 const TopicCard: React.FC<TopicCardProps> = ({ id, title, summary, date, likes, image, owner, type }) => {
     return (
         <Link href={`/contentpage/${id}`} passHref>
-            <div className="block border rounded-lg p-4 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 max-w-sm cursor-pointer">
-                <img src={image} alt={title} className="w-full h-20 md:w-80 md:h-60 object-cover rounded-md" />
-                <div className="flex flex-col justify-between">
+            <div className="block border p-4 flex flex-col space-y-4 max-w-lg cursor-pointer">
+                <img src={image} alt={title} className="w-full h-40 object-cover rounded-md" />
+                <div className="flex flex-col w-full">
                     <div>
                         <h3 className="text-lg font-semibold text-black">{title}</h3>
                         <p className="text-sm text-gray-500">{date}</p>
                         <p className="mt-2 text-gray-700">{summary}</p>
                     </div>
-                    <div>
-                        <p className="mt-2 text-gray-700">{likes.toLocaleString()} likes</p>
-                        <p className="mt-2 text-gray-700">Owner: {owner}</p>
-                        <p className="mt-2 text-gray-700">Type: {type}</p>
+                    <div className="flex flex-wrap gap-4 mt-2">
+                        <p className="text-gray-700">{likes.toLocaleString()} likes</p>
+                        <p className="text-gray-700">Owner: {owner}</p>
+                        <p className="text-gray-700">Type: {type}</p>
                     </div>
                 </div>
             </div>
