551cddeb892baf4fd8817cc711d8e841eed5965f
Angelo Santos <gabrielange883@gmail.com>
2024-08-08 13:26:18 -0300

    08082024-Explorer Adjustements

--- retriever/src/pages/explorer.tsx
+++ retriever/src/pages/explorer.tsx
@@ -6,6 +6,7 @@ import TopicCard from '../components/TopicCard';
 import { getAllPosts } from '../../sanity/lib/sanity';
 import '../app/globals.css';
 import React from 'react';
+import Link from "next/link";
 
 interface Post {
     id: string;
@@ -26,7 +27,7 @@ const Explorer: React.FC<ExplorerProps> = ({ posts }) => {
     return (
         <main className="bg-white flex min-h-screen flex-col items-center justify-between p-0">
             <Head>
-                <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet"/>
                 <title>Explorar tópicos</title>
             </Head>
 
@@ -34,9 +35,10 @@ const Explorer: React.FC<ExplorerProps> = ({ posts }) => {
                 <Nav />
             </div>
 
-            <div className="container mx-auto p-4 mt-40">
-                <h1 className="text-2xl font-bold mb-6">Explorar</h1>
-                <div className="space-y-20">
+            <div className="container mx-auto p-4 mt-40 flex">
+                {/* Coluna dos Cards */}
+                <div className="flex-1 space-y-20">
+                    <h1 className="text-4xl font-bold mb-6">Explorar</h1>
                     {posts.map((post) => (
                         <TopicCard
                             key={post.id}
@@ -50,6 +52,31 @@ const Explorer: React.FC<ExplorerProps> = ({ posts }) => {
                             type={post.type}
                         />
                     ))}
+                </div>
+
+                
+                <div className="w-1/4 p-4 space-y-6 ml-8">
+                    <div>
+                        <div className="text-black font-semibold">
+                            Artigos e Referências
+                        </div>
+                        <ul className="space-y-1 text-gray-400">
+                            <li><a href="#" className="text-sm hover:underline">Contatos</a></li>
+                            <li><Link href="/about" className="text-sm hover:underline">Sobre nós</Link></li>
+                            <li><Link href="/suport" className="text-sm hover:underline">Ajuda</Link></li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <div className="text-black font-semibold">
+                            Sei lá
+                        </div>
+                        <ul className="space-y-1 text-gray-400">
+                            <li><a href="#" className="text-sm hover:underline">Contatos</a></li>
+                            <li><Link href="/about" className="text-sm hover:underline">Sobre nós</Link></li>
+                            <li><Link href="/suport" className="text-sm hover:underline">Ajuda</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
 
