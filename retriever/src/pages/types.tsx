e0efd27091a79ebabf38493fa230c70413045763
Angelo Santos <gabrielange883@gmail.com>
2024-08-08 13:42:46 -0300

    08082024-Types Adjustements

--- retriever/src/pages/types.tsx
+++ retriever/src/pages/types.tsx
@@ -13,7 +13,7 @@ const Types = () => {
     };
 
     return (
-        <div className="min-h-screen bg-gray-100">
+        <div className="min-h-screen bg-white">
             <Head>
                 <title>Tipos de Ansiedade</title>
             </Head>
@@ -25,10 +25,10 @@ const Types = () => {
                 </div>
             </header>
 
-            <main className="container mx-auto p-4">
-                <section className="mb-8">
-                    <h2 className="text-4xl text-black text-center font-semibold mt-40">Ansiedade Classificada por Tipos</h2>
-                    <h2 className="text-xl font-medium mb-4 text-center text-gray-800 mt-6">
+            <main className="container mx-auto p-4 mb-16"> 
+                <section className="mb-8 text-center">
+                    <h2 className="text-4xl text-black font-semibold mt-40">Ansiedade Classificada por Tipos</h2>
+                    <h2 className="text-xl font-medium mb-4 text-gray-800 mt-6">
                         Todo mundo já sentiu apreensão e ansiedade em algum momento da vida. Seja pela expectativa de uma prova, a apresentação de um projeto ou algum compromisso importante. Essa é a ansiedade comum do dia a dia, da qual tanto ouvimos falar. Mas você sabia que existem diferentes tipos de ansiedade?
                     </h2>
 
@@ -37,7 +37,7 @@ const Types = () => {
                             <button
                                 key={index}
                                 className={`p-4 text-lg transition duration-300 font-semibold ease-in-out ${
-                                    selectedType === type ? 'border-b-4 border-black text-black' : 'text-back hover:text-black'
+                                    selectedType === type ? 'border-b-4 border-black text-black' : 'text-white hover:text-black'
                                 }`}
                                 onClick={() => handleTypeClick(type)}
                             >
@@ -47,66 +47,52 @@ const Types = () => {
                     </div>
                 </section>
 
-                <section>
+                <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     {selectedType === 'Social' && (
                         <Link href="/about">
-                            <article
-                                className="mb-8 bg-white shadow-lg rounded-lg overflow-hidden max-w-sm mx-auto p-4">
+                            <article className="bg-white rounded-lg shadow-md overflow-hidden max-w-sm mx-auto">
                                 <img src="/ImagesRetriever/ansiedadesocialtypes.jpg"
                                      alt="Transtorno de Ansiedade Social"
-                                     className="w-full h-64 object-cover mb-4 shadow-md rounded-t-lg"/>
+                                     className="w-full h-64 object-cover mb-4"/>
                                 <div className="px-4">
-                                    <h3 className="text-xl font-semibold text-black mb-2">Transtorno de Ansiedade
-                                        Social: o
-                                        que é, sintomas e tratamento</h3>
-                                    <p className="text-gray-700">transtorno de ansiedade
-                                        social.</p>
+                                    <h3 className="text-xl font-semibold text-black mb-2">Transtorno de Ansiedade Social: o que é, sintomas e tratamento</h3>
+                                    <p className="text-gray-700">Transtorno de ansiedade social.</p>
                                 </div>
                             </article>
                         </Link>
                     )}
                     {selectedType === 'Pânico' && (
                         <Link href="/about">
-                            <article
-                                className="mb-8 bg-white shadow-lg rounded-lg overflow-hidden max-w-sm mx-auto p-4">
+                            <article className="bg-white rounded-lg shadow-md overflow-hidden max-w-sm mx-auto">
                                 <img src="/ImagesRetriever/panicotypes.jpg" alt="Ansiedade em Crianças"
-                                     className="w-full h-64 object-cover mb-4 shadow-md rounded-t-lg"/>
+                                     className="w-full h-64 object-cover mb-4"/>
                                 <div className="px-4">
-                                    <h3 className="text-xl font-semibold text-black mb-2">Entenda o que é ansiedade
-                                        social e como se
-                                        manifesta nas crianças</h3>
-                                    <p className="text-gray-700">ansiedade em crianças.</p>
+                                    <h3 className="text-xl font-semibold text-black mb-2">Entenda o que é ansiedade social e como se manifesta nas crianças</h3>
+                                    <p className="text-gray-700">Ansiedade em crianças.</p>
                                 </div>
                             </article>
                         </Link>
-
                     )}
                     {selectedType === 'TOC' && (
                         <Link href="/about">
-                            <article
-                                className="mb-8 bg-white shadow-lg rounded-lg overflow-hidden max-w-sm mx-auto p-4">
+                            <article className="bg-white rounded-lg shadow-md overflow-hidden max-w-sm mx-auto">
                                 <img src="/ImagesRetriever/toctypes.jpg" alt="Ansiedade e Fobia Social"
-                                     className="w-full h-64 object-cover mb-4 shadow-md rounded-t-lg"/>
+                                     className="w-full h-64 object-cover mb-4"/>
                                 <div className="px-4">
-                                    <h3 className="text-xl font-semibold text-black mb-2">Fobia social quando o
-                                        sofrimento pela timidez é tanto que a saída é se esconder</h3>
-                                    <p className="text-gray-700">ansiedade e fobia social.</p>
+                                    <h3 className="text-xl font-semibold text-black mb-2">Fobia social quando o sofrimento pela timidez é tanto que a saída é se esconder</h3>
+                                    <p className="text-gray-700">Ansiedade e fobia social.</p>
                                 </div>
                             </article>
                         </Link>
-
                     )}
                     {selectedType === 'TDAH' && (
                         <Link href="/about">
-                            <article
-                                className="mb-8 bg-white shadow-lg rounded-lg overflow-hidden max-w-sm mx-auto p-4">
+                            <article className="bg-white rounded-lg shadow-md overflow-hidden max-w-sm mx-auto">
                                 <img src="/ImagesRetriever/tdahtypes.jpeg" alt="Ansiedade e Fobia Social"
-                                     className="w-full h-64 object-cover mb-4 shadow-md rounded-t-lg"/>
+                                     className="w-full h-64 object-cover mb-4"/>
                                 <div className="px-4">
-                                    <h3 className="text-xl font-semibold text-black mb-2">Fobia social quando o
-                                        sofrimento
-                                        pela timidez é tanto que a saída é se esconder</h3>
-                                    <p className="text-gray-700">ansiedade e fobia social.</p>
+                                    <h3 className="text-xl font-semibold text-black mb-2">Fobia social quando o sofrimento pela timidez é tanto que a saída é se esconder</h3>
+                                    <p className="text-gray-700">Ansiedade e fobia social.</p>
                                 </div>
                             </article>
                         </Link>
@@ -114,7 +100,7 @@ const Types = () => {
                 </section>
             </main>
 
-            <Footer/>
+            <Footer />
         </div>
     );
 };
