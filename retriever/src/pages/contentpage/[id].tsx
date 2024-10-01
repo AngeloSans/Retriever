14669e1006c698f65a887a03d1f5e652e931bf97
Anjocaido13 <gabrielange883@gmail.com>
2024-10-01 18:36:50 -0300

    adding image

--- retriever/src/pages/contentpage/[id].tsx
+++ retriever/src/pages/contentpage/[id].tsx
@@ -1,17 +1,15 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { client } from '../../../sanity/lib/sanity'; 
 import { useRouter } from 'next/router';
-
-interface PostImage {
-  asset: {
-    url: string;
-  };
-}
+import Nav from '@/components/Nav';
+import Footer from '@/components/Footer';
+import Image from "next/image";
 
 interface Post {
   _id: string;
   title: string;
   summary: string;
+  image: string; 
 }
 
 interface PostPageProps {
@@ -26,43 +24,62 @@ const PostPage = ({ post }: PostPageProps) => {
   }
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <div>{post.summary}</div>
-    </div>
+    <main className="bg-white flex min-h-screen flex-col items-center justify-between p-0 font-sans">
+      <Nav />
+      <div className="mt-16">
+        <h1 className="mt-20">{post.title}</h1>
+        <div className="text-black">{post.summary}</div>
+        {post.image && (
+          <Image
+            src={post.image} 
+            alt={post.title}
+            width={500} 
+            height={400} 
+            className="mt-4" 
+          />
+        )}
+      </div>
+      <Footer />
+    </main>
   );
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const query = `*[_type == "post"]{ _id }`;
-    const posts = await client.fetch(query);
-  
-    const paths = posts.map((post: { _id: string }) => ({
-      params: { id: post._id },
-    }));
-  
-    return {
-      paths,
-      fallback: true,
-    };
+  const query = `*[_type == "post"]{ _id }`;
+  const posts = await client.fetch(query);
+
+  const paths = posts.map((post: { _id: string }) => ({
+    params: { id: post._id },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
 };
-  
+
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const query = `*[_type == "post" && _id == $id][0]`;
-    const post = await client.fetch(query, { id: params?.id });
+  const query = `*[_type == "post" && _id == $id][0]{
+    _id, 
+    title, 
+    summary, 
+    "image": image.asset->url // Corrigido para 'url'
+  }`;
   
-    if (!post) {
-      return {
-        notFound: true,
-      };
-    }
-  
+  const post = await client.fetch(query, { id: params?.id });
+
+  if (!post) {
     return {
-      props: {
-        post,
-      },
-      revalidate: 60,
+      notFound: true,
     };
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60,
+  };
 };
-  
+
 export default PostPage;
